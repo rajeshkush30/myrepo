280c4b1a60d6c17a8d67f0e8f9605dcd5c54345b
Rajesh Kushwaha <rajeshrsk30@gmail.com>
2024-10-03 19:03:29 +0530

    this is v3

--- src/main/java/org/example/Counter.java
+++ src/main/java/org/example/Counter.java
@@ -1,2 +1,12 @@
-package org.example;public class Counter {
+package org.example;
+
+public class Counter {
+    public void count(){
+        System.out.println();
+        for(int i=1;i<=100; i++){
+
+            System.out.print(i+", ");
+
+        }
+    }
 }
